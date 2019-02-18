8ca58fe3e452598f0bd1a66d685189828b2231bc
Eugene Mamonov <chipmob@gmail.com>
2019-02-18 16:40:40 +0200

    update

---
+++ custom.js
@@ -0,0 +1,32 @@
+
+(function(referer) {
+    if (referer == '') return;
+
+    let forms = document.getElementsByTagName("form");
+    for (let i = 0; i < forms.length; i++) {
+        let input = document.createElement("input");
+        input.type = "hidden";
+        input.name = "referer";
+        input.value = referer;
+        forms[i].appendChild(input);
+    }
+}(document.referrer));
+
+(function(channel) {
+    if (channel == '') return;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://kshop5.pro/" + channel);
+    xhr.setRequestHeader('X-Kma-Api', 'click');
+    xhr.send();
+    xhr.onload = function() {
+        let forms = document.getElementsByTagName("form");
+        for (let i = 0; i < forms.length; i++) {
+            let input = document.createElement("input");
+            input.type = "hidden";
+            input.name = "click";
+            input.value = this.responseText;
+            forms[i].appendChild(input);
+        }
+    };
+}('9yJqn3'));
