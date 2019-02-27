f7b4596072e268a3ec041e0864d4d948fc8c35c8
Eugene Mamonov <chipmob@gmail.com>
2019-02-27 10:20:51 +0200

    update

--- success.php
+++ success.php
@@ -4,25 +4,13 @@ if (is_file('config.php')) {
     require_once 'config.php';
 }
 
+$token = defined('KMA_ACCESS_TOKEN') ? KMA_ACCESS_TOKEN : 'access token';
+$channel = defined('KMA_CHANNEL') ? KMA_CHANNEL : 'channel';
 $debug = defined('KMA_DEBUG') ? KMA_DEBUG : false;
 
 if ($debug) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-}
-
-$token = defined('KMA_ACCESS_TOKEN') ? KMA_ACCESS_TOKEN : 'access token';
-$channel = defined('KMA_CHANNEL') ? KMA_CHANNEL : 'channel';
-
-$data = [
-    'channel' => $channel,
-    'ip' => $_SERVER['REMOTE_ADDR'],
-];
-
-foreach (['name', 'phone', 'data1', 'data2', 'data3', 'data4', 'data5', 'click', 'referer', 'return_page'] as $item) {
-    if (isset($_POST[$item]) && !empty($_POST[$item])) {
-        $data[$item] = $_POST[$item];
-    }
 }
 
 require_once 'KmaLead.php';
@@ -33,6 +21,19 @@ $kma = new KmaLead($token);
 if (isset($_SERVER['HTTP_X_KMA_API']) && $_SERVER['HTTP_X_KMA_API'] === 'click') {
     echo $kma->getClick($channel);
     exit();
+} elseif ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    exit();
+}
+
+$data = [
+    'channel' => $channel,
+    'ip' => $_SERVER['REMOTE_ADDR'],
+];
+
+foreach (['name', 'phone', 'data1', 'data2', 'data3', 'data4', 'data5', 'click', 'referer', 'return_page'] as $item) {
+    if (isset($_POST[$item]) && !empty($_POST[$item])) {
+        $data[$item] = $_POST[$item];
+    }
 }
 
 $kma->debug = $debug;
