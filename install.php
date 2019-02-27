a9e7fbb4c5d432ce4f13e56c5b7ddd3a3935413d
Eugene Mamonov <chipmob@gmail.com>
2019-02-27 11:39:54 +0200

    update

--- install.php
+++ install.php
@@ -2,28 +2,29 @@
 
 $check = [];
 if (phpversion() < '5.4') {
-    $check[] = 'You need to use PHP5.4+ or above for API work';
+    $check[] = 'Версия PHP должна быть >5.4 для работы скриптов API';
 }
 if (!extension_loaded('curl')) {
-    $check[] = 'CURL extension needs to be loaded for API work';
+    $check[] = 'Расширение CURL должно быть загружено для работы по API';
+}
+if (!is_writable('install.php')) {
+    $check[] = 'Сервер не позволяет записывать файлы';
 }
 if (!empty($check)) {
-    echo "<h1>Для работы API необходимо устранить следующие проблемы:</h1>";
+    echo "<p>Для работы API необходимо устранить следующие проблемы:</p>";
     echo "<pre>"; echo var_export($check, true); echo "</pre>";
     exit();
 }
 
 if (is_file('config.php')) {
-    echo 'CONFIG EXIST';
-    exit();
+    $exit = (isset($_GET['action']) && $_GET['action'] === 'done') ? 'CONFIG SAVED' : 'CONFIG EXIST';
+    exit($exit);
 }
 
 $valid = true;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $valid = isset($_POST['token']) && !empty($token = $_POST['token']) && isset($_POST['channel']) && !empty($channel = $_POST['channel']);
-    if ($valid) {
-        $valid = preg_match('/^\w{6}$/', $channel) && preg_match('/^[\w-]{32}$/', $token);
-        if ($valid) {
+    if (isset($_POST['token']) && !empty($token = $_POST['token']) && isset($_POST['channel']) && !empty($channel = $_POST['channel'])) {
+        if (preg_match('/^[\w-]{32}$/', $token) && preg_match('/^\w{6}$/', $channel)) {
             $output  = "<?php" . PHP_EOL . PHP_EOL;
             $output .= "define('KMA_ACCESS_TOKEN', '$token');" . PHP_EOL;
             $output .= "define('KMA_CHANNEL', '$channel');" . PHP_EOL;
@@ -31,15 +32,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $file = fopen('config.php', 'w');
             fwrite($file, $output);
             fclose($file);
-            echo 'CONFIG SAVED';
+            header('Location: install.php?action=done');
             exit();
         } else {
+            $valid = false;
             $message = 'API ключ или поток не могут принимать данные значения';
         }
     } else {
+        $valid = false;
         $message = 'API ключ и поток не могут быть пустыми';
     }
 }
 
-include_once 'setup.php';
+include_once 'tpl_setup.php';
+
 exit();
