ff5f91f75ccb79aeb08534e245486ec6ac0917d2
Eugene Mamonov <chipmob@gmail.com>
2019-10-16 13:51:18 +0300

    update

--- _session.php
+++ _session.php
@@ -1,4 +1,5 @@
 <?php
+$order = $country = $name = $surname = $phone = $price = $currency = '-';
 foreach (['order', 'country', 'name', 'surname', 'phone', 'price', 'currency'] as $item) {
     $$item = isset($_SESSION[$item]) ? $_SESSION[$item] : '-';
 }
