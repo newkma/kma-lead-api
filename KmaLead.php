9d50fde2c1c80e6c7c5b29fc9dceb251dfd246cf
Eugene Mamonov <chipmob@gmail.com>
2021-03-22 10:17:57 +0200

    update

--- KmaLead.php
+++ KmaLead.php
@@ -85,7 +85,9 @@ class KmaLead
             curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLINFO_HEADER_OUT, true);
-            curl_setopt($curl,CURLOPT_TIMEOUT,15);
+            curl_setopt($curl, CURLOPT_TIMEOUT,15);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             $result = curl_exec($curl);
             if (curl_errno($curl) && in_array(curl_errno($curl), [CURLE_OPERATION_TIMEDOUT, CURLE_OPERATION_TIMEOUTED])) {
                 try {
@@ -113,7 +115,9 @@ class KmaLead
             curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLINFO_HEADER_OUT, true);
-            curl_setopt($curl,CURLOPT_TIMEOUT,5);
+            curl_setopt($curl, CURLOPT_TIMEOUT,5);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             $result = curl_exec($curl);
             curl_close($curl);
             return $result;
